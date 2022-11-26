0d5bd63f702ea25e53393dc59ed9e0be758726fa
jeus5771 <jeus5771@naver.com>
2022-11-26 19:36:51 +0900

    2022-11-26

--- app/src/main/cpp/7segment.c
+++ app/src/main/cpp/7segment.c
@@ -5,3 +5,39 @@
 #include <sys/ioctl.h>
 #include <android/log.h>
 
+/* 현재까지의 누적 coin을 표시하기 위해서 coin 개수를 매개변수 jint data로 받아서
+   디바이스에 쓰기 위한 c/c++ 코드 구현 */
+
+JNIEXPORT jint JNICALL
+Java_com_example_a7segmentcontrol_MainActivity_SSegmentWrite(JNIEnv *env, jobject thiz, jint data) {
+    int fd, ret;
+    unsigned char bytevalues[4];
+
+    if((data < 0) || (data > 9999)) {
+        printf("Invalid 7segment data range!\n");
+        return -1;
+    }
+    fd = open("/dev/7segment", O_WRONLY);
+    if (fd < 0) {
+        __android_log_print(ANDROID_LOG_ERROR, "SSegmentWrite","7segment device file open error");
+        return -1;
+    }
+
+    bytevalues[0] = data / 1000;
+    data = data % 1000;
+    bytevalues[1] = data / 100;
+    data = data % 100;
+    bytevalues[2] = data / 10;
+    data = data % 10;
+    bytevalues[3] = data;
+
+    ret = write(fd, bytevalues, 4);
+    if (ret < 0) {
+        __android_log_print(ANDROID_LOG_ERROR,"SSegmentWrite","7Segment write error");
+        return -1;
+    }
+
+    close(fd);
+
+    return 0;
+}
--- app/src/main/cpp/lcd.c
+++ app/src/main/cpp/lcd.c
@@ -1,6 +1,57 @@
-#include <jni.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <jni.h>
 #include <android/log.h>
+#include <stdbool.h>
+
+#define LCD_MAGIC	0xBD            // magic number setting 필요
+#define LCD_SET_CURSOR_POS	_IOW(LCD_MAGIC, 0 ,int)
+
+/* 게임 시작과 종료를 lcd device에 표현하기 위한 함수 구현 */
+/* 매개변수로 true가 들어오면 게임 시작 메시지 출력
+   매개변수로 false가 들어오면 게임 종료 메시지를 출력 */
+
+JNIEXPORT jint JNICALL
+Java_com_example_a7segmentcontrol_MainActivity_LcdWrite(JNIEnv *env, jobject thiz, jboolean check)
+{
+    int fd, pos;
+    char game_start[2][16] = {"game start","enjoy your game"};
+    char game_end[2][16] = {"game end","press restart"};
+
+    fd = open("/dev/lcd", O_WRONLY);
+    if (fd < 0) {
+        __android_log_print(ANDROID_LOG_ERROR, "LcdWrite","Lcd device file open error");
+        return -1;
+    }
+
+    switch(check) {
+        case true:
+            pos = 0;
+            ioctl(fd, LCD_SET_CURSOR_POS, &pos, _IOC_SIZE(LCD_SET_CURSOR_POS));
+            write(fd, game_start[0], strlen(game_start[0]));
+
+            pos = 16;
+            ioctl(fd, LCD_SET_CURSOR_POS, &pos, _IOC_SIZE(LCD_SET_CURSOR_POS));
+            write(fd, game_start[1], strlen(game_start[1]));
+            break;
+        case false:
+            pos = 0;
+            ioctl(fd, LCD_SET_CURSOR_POS, &pos, _IOC_SIZE(LCD_SET_CURSOR_POS));
+            write(fd, game_end[0], strlen(game_end[0]));
+
+            pos = 16;
+            ioctl(fd, LCD_SET_CURSOR_POS, &pos, _IOC_SIZE(LCD_SET_CURSOR_POS));
+            write(fd, game_end[1], strlen(game_end[0]));
+            break;
+    }
+
+    close(fd);
+
+    return 0;
+}
